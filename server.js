8592fc294503f3b075a97e07fc9098310dcd26af
miku-nicol <miracleuwaifo9@gmail.com>
2026-04-24 02:34:16 +0100

    change my start script

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) =>{
  
 
 connectDB()
-const PORT= process.env.PORT;
+const PORT= process.env.PORT || 9000
 
 app.listen(PORT, () =>{
     console.log(`server running on http://localhost:${PORT}`)
