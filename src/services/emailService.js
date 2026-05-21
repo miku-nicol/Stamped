65ecccef5876a29e86e38b0d2a9cbc6c001db07d
miku-nicol <miracleuwaifo9@gmail.com>
2026-05-21 20:44:26 +0100

    still fixing email transpoter

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -11,7 +11,10 @@ const transporter = nodemailer.createTransport({
   },
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeout: 10000, // 10 sec
+  greetingTimeout: 10000,
+  socketTimeout: 15000
 });
 
 const sendPasswordResetEmail = async (email, resetToken, userName) => {
